4047ff433d59dedb7c9cbe2e9900dadf6180be62
Jennifer <100975749+Jennifer328@users.noreply.github.com>
2023-11-24 12:09:51 -0500

    first commit

---
+++ src/pages/search/Search.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import { useLocation } from 'react-router-dom';
+
+import './Search.css';
+import { useFetch } from '../../hooks/useFetch';
+import RecipeList from '../../components/RecipeList';
+
+export default function Search() {
+
+  const queryString = useLocation().search;
+  const queryParams = new URLSearchParams(queryString);
+  const query = queryParams.get("q");  //?q=###
+  
+  const url = ` http://localhost:3000/recipes?q=` + query;
+  
+  const {error, isPending,data} = useFetch(url)
+
+
+  return (
+    <div>
+      <h2 className="page-title">Receipes including "{query}"</h2>
+      {error && <p className='error'>{error}</p>}
+      {isPending && <p className='loading'>Loading...</p>}
+      {data && <RecipeList recipes={data}/>}
+    </div>
+  )
+}
